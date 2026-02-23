c7ad70b9d01da8ae77b827b05b764ab937d7a9b5
Md. Sadiq Ibn Mas-Ud <sajidnsubd@gmail.com>
2026-02-23 12:47:12 +0600

    updated auth-client from lib

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,17 @@
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 
+// export const authClient = createAuthClient({
+//   baseURL: env.NEXT_PUBLIC_API_URL,
+// });
+
+// trying solution cookies after deployment
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_API_URL,
+  // ✅ Add fetch options for cross-domain cookies
+  fetchOptions: {
+    credentials: "include", // Send cookies with cross-domain requests
+  },
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;

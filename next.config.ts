57c58cea117ff743b853b96cc0e59c151dd099e0
Md. Sadiq Ibn Mas-Ud <sajidnsubd@gmail.com>
2026-05-05 19:50:45 +0600

    image errpr fix

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
     formats: ["image/webp"],
   },
